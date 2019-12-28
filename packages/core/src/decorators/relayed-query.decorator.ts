ef397b85bf1f8d7b2f8829a58faf80c9b90e2045
David <superd001@gmail.com>
2019-12-28 13:41:39 +0100

    feat: allow better return type for RelayedQueryFunction (#20)
    closes #3

--- packages/core/src/decorators/relayed-query.decorator.ts
+++ packages/core/src/decorators/relayed-query.decorator.ts
@@ -3,8 +3,8 @@ import { ClassValueThunk } from '../'
 import { AdvancedOptions } from 'type-graphql/dist/decorators/types'
 import { RelayedQueryService } from '../services/relay-query.service';
 
-export function RelayedQuery<Model = any>(to: ClassValueThunk<Model>): RelayedQueryFactory<Model, false>
-export function RelayedQuery<Model = any>(to: ClassValueThunk<Model>, options: RelayedQueryOptions): RelayedQueryFactory<Model, false>
+export function RelayedQuery<Model = any, Through = undefined>(to: ClassValueThunk<Model>): RelayedQueryFactory<Model, false>
+export function RelayedQuery<Model = any, Through = undefined>(to: ClassValueThunk<Model>, options: RelayedQueryOptions): RelayedQueryFactory<Model, false>
 export function RelayedQuery<Model = any, Through = any>(to: ClassValueThunk<Model>, through: ClassValueThunk<Through>): RelayedQueryFactory<Through, true>
 export function RelayedQuery<Model = any, Through = any>(to: ClassValueThunk<Model>, through: ClassValueThunk<Through>, options: RelayedQueryOptions): RelayedQueryFactory<Through, true>
 export function RelayedQuery<Model = any, Through = any>(to: ClassValueThunk<Model>, through?: ClassValueThunk<Through> | RelayedQueryOptions, options?: RelayedQueryOptions): RelayedQueryFactory<Through extends (undefined | null) ? Model : Through, Through extends (undefined | null) ? false : true> {
@@ -14,15 +14,10 @@ export function RelayedQuery<Model = any, Through = any>(to: ClassValueThunk<Mod
     through = undefined;
   }
 
-  return (
+  return <ActualModel = Through extends (undefined | null) ? Model : Through, HasThrough extends boolean =Through extends (undefined | null) ? never : true>(
     target: Object,
     propertyKey: string,
-    descriptor: TypedPropertyDescriptor<
-      RelayedQueryGetterFunction<
-        Through extends (undefined | null) ? Model : Through,
-        Through extends (undefined | null) ? never : true
-      >
-    >
+    descriptor: PropertyDescriptor
   ) => {
     process.nextTick(() => {
       Container.get(RelayedQueryService).makeMethodRelayedQuery(target, propertyKey, descriptor, to, through as  ClassValueThunk<Through> | undefined, options)
@@ -30,10 +25,37 @@ export function RelayedQuery<Model = any, Through = any>(to: ClassValueThunk<Mod
   }
 }
 
-export type RelayedQueryGetterFunction<Model = any, U = false> = (...arg: any[]) => U extends true ? Promise<[number, Model[], keyof Model]> : Promise<[number, Model[]]>;
-export type RelayedQueryFactory<T = unknown, U = false> = (target: Object, propertKey: string, descriptor: TypedPropertyDescriptor<RelayedQueryGetterFunction<T, U>>) => void
+export type RelayedQueryTypedDescriptor<ActualModel=any, HasThrough extends boolean = false> = 
+| RelayedQueryGetterFunction<[number, ActualModel[], HasThrough extends true ? keyof ActualModel : void]>
+| RelayedQueryGetterFunction<[ActualModel[], number, HasThrough extends true ? keyof ActualModel : void]>
+| RelayedQueryGetterFunction<RelayEntityWithCounts<ActualModel, HasThrough>>
+
+export type RelayedQueryGetterFunction<Y> = (...arg: any[]) => Promise<Y>
+
+export type RelayedQueryFactory<ActualModel = unknown, HasThrough extends boolean = false> =
+(
+   target: Object,
+   propertKey: string,
+   descriptor: 
+    | (HasThrough extends true ? TypedPropertyDescriptor<RelayedQueryGetterFunction<[ActualModel[], number, keyof ActualModel]>> : TypedPropertyDescriptor<RelayedQueryGetterFunction<[ActualModel[], number]>>)
+    | (HasThrough extends true ? TypedPropertyDescriptor<RelayedQueryGetterFunction<[number, ActualModel[], keyof ActualModel]>> : TypedPropertyDescriptor<RelayedQueryGetterFunction<[number, ActualModel[]]>>)
+    | (TypedPropertyDescriptor<RelayedQueryGetterFunction<RelayEntityWithCounts<ActualModel, HasThrough>>>)
+    ) => void
 
 export type RelayedQueryOptions = RelayLimitOffsetOptions & AdvancedOptions;
+
+/**
+ * Object to return to build a relay'd query
+ */
+export interface RelayEntityWithCounts<Model = any, HasThrough extends boolean = false> {
+  /** total amount of matching results */
+  count: number
+  /** actual rows to be returned for pagination */
+  rows: Model[]
+  /** key when using a through */
+  key?: HasThrough extends true ? keyof Model : undefined
+}
+
 export interface RelayLimitOffsetOptions {
   /** 
    * set to true if you don't want to use an input type instead of inlining pagination args
--- packages/core/src/middlewares/relay-from-array-count.middleware.ts
+++ packages/core/src/middlewares/relay-from-array-count.middleware.ts
@@ -1,9 +1,12 @@
-import { AutoRelayRoot } from './../interfaces/auto-relay-root-metadata.interface';
-import { RelayLimitOffsetResolverFactory } from './../decorators/limit-offset.decorator';
-import { ResolverData, NextFn } from "type-graphql";
-import { connectionArgsFromResolverData } from '../helpers/pagination-args-from-data.function';
-import { augmentedConnection } from '../helpers/augment-connection.function';
+import { RelayedQueryGetterFunction, RelayEntityWithCounts, RelayedQueryTypedDescriptor } from './../decorators/relayed-query.decorator'
+import { AutoRelayRoot } from './../interfaces/auto-relay-root-metadata.interface'
+import { RelayLimitOffsetResolverFactory } from './../decorators/limit-offset.decorator'
+import { ResolverData, NextFn } from "type-graphql"
+import { connectionArgsFromResolverData } from '../helpers/pagination-args-from-data.function'
+import { augmentedConnection } from '../helpers/augment-connection.function'
 import * as Relay from 'graphql-relay'
+
+type UnPromisify<T> = T extends Promise<infer U> ? U : T;
 
 /**
  * Middleware function factory to be used on any RelayField/RelayQuery whose methods
@@ -14,9 +17,19 @@ import * as Relay from 'graphql-relay'
  * @param prototype the prototype of our resolver
  * @param propertyKey key on the resolver we're accessing
  */
-export function RelayFromArrayCountFactory<TContext = {}>(prototype: any, propertyKey: string) {
-  return async <Model = any, Through = any>(resolverData: ResolverData<TContext>, next: NextFn) => {
-    const [entityCount, entities, throughKey] = (await next() as [number, Array<Model | Through>, keyof Through | undefined]);
+export function RelayFromArrayCountFactory<
+  TContext = {},
+  Model=any,
+  HasThrough extends boolean = false
+> ( 
+  prototype: any,
+  propertyKey: string
+) {
+  return async (
+    resolverData: ResolverData<TContext>,
+    next: RelayedQueryTypedDescriptor<Model, HasThrough>
+  ) => {
+    const [entityCount, entities, throughKey] = extractGetterFunctionReturn(await next())
 
     const args = connectionArgsFromResolverData(prototype, propertyKey, resolverData);
     const pagination = RelayLimitOffsetResolverFactory(prototype, propertyKey)(resolverData);
@@ -32,5 +45,37 @@ export function RelayFromArrayCountFactory<TContext = {}>(prototype: any, proper
     } else {
       return connection;
     }
+  }
+}
+
+
+export function extractGetterFunctionReturn<Model = any, U extends boolean = false>(
+  payload: UnPromisify<ReturnType<RelayedQueryTypedDescriptor<Model, U>>>
+): [number, Model[], (keyof Model)?] {
+
+  if (Array.isArray(payload)) {
+    if (typeof payload[0] === "number")  {
+      return payload as [number, Model[], (keyof Model)?]
+    } else if(typeof payload[1] === "number") {
+      const [models, count, key] = payload as [Model[],number,(keyof Model)?]
+      return [count, models, key]
+    } else {
+      throw new WrongReturnType(`got [${typeof payload[0]}, ${typeof payload[1]}, ${typeof payload[2]}] instead of expected [number, entity[]] or [entity[], number] }`)
+    }
+  } else {
+    const { count, rows, key } = payload
+
+    if (typeof count !== "number") throw new WrongReturnType(`count is supposed to be number, got ${count} instead`)
+    if (!Array.isArray(rows)) throw new WrongReturnType(`rows is supposed to be array, got ${rows} instead`)
+
+    return [count, rows, key]
+  }
+
+}
+
+
+export class WrongReturnType extends Error {
+  constructor(message: string) {
+    super(`Wrong return type for Relayed method: ${message}`)
   }
 }
